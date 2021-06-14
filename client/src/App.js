f29a5b70a8453c967737ac812902e40bec33b3ae
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-06-14 12:54:18 -0400

    all packages are installed

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import novelCollection from './components/Novels/novels';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      < novelCollection />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Novels/novels.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './novels.css'
+
+
+const novelCollection = () => {
+    return (
+        <>
+        <div className="navbar-container">
+            <nav className="navbar">
+                <h1 className="title">  
+                Chadwick's Novels
+                </h1>
+            </nav>
+             
+        </div>
+        </>
+
+    )
+}
+export default novelCollection
