2d45a28892fae2dcf5ef2d386fe7fc11446295cc
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-06-14 13:13:36 -0400

    fetch function is made

---
+++ client/src/utils/Novels.js
@@ -0,0 +1,13 @@
+import fetchApiData from '../actions/Novels';
+
+export async function fetchData() {
+    const data = await fetchApiData()
+
+    return data
+}
+
+export const filtered = (novelsData, searchTerm) => novelsData.filter(novels => {
+    return novels.name.toLowerCase().includes(searchTerm.toLowerCase)
+})
+//novlesData
+//searchTerm
