7308d66792ffabef968e3e423fdcc6353f31bbf1
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-06-21 16:23:12 -0400

    added an Error component

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Error.css'
+import './novels.css'
+
+const Error = () => {
+    return (
+    <html>
+
+  <head>
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+
+  <body>
+    <h2>Page Not Found</h2>
+    <h3>¯\_(ツ)_/¯</h3>
+  <div className='title'>
+  <h1 className='title-name'> Great Novels </h1>
+  </div>
+  </body>
+
+</html>
+    )
+}
+
+export default Error 
